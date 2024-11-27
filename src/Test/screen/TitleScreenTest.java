a2cf23793542a3beb5b8b197c0e50d4590995bd3
sw02020 <cowper02@naver.com>
2024-11-27 14:28:45 +0900

    KAN-74 feat: add test_code

---
+++ src/Test/screen/TitleScreenTest.java
@@ -0,0 +1,19 @@
+package screen;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TitleScreenTest {
+    @Nested
+    class test_KAN_74_HSW{
+        @Test
+        void TitleScreen() {
+        }
+
+        @Test
+        void draw() {
+        }
+    }
+}
